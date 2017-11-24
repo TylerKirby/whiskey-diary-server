ad05f50fa51b371155c5e9c9b8e5db0bb039b051
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-11-24 12:17:15 -0600

    added dotenv and mysql connection

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mysql from 'mysql';
 
 const app = express();
 
 app.set('port', 3000);
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  "host": process.env.HOST,
+  "user": process.env.USER,
+  "password": process.env.PASSWORD
+});
+
+connection.connect(error => { error ? console.log(error) : console.log('Succesfully connected to database.')});
